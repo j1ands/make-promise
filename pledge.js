bb8174847389527c3966ac843b877e55be375047
Jordan Landau <jlandau@uoregon.edu>
2014-12-08 10:57:56 -0500

    finished chapter 1

--- pledge.js
+++ pledge.js
@@ -3,7 +3,44 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise()
+{
 
+}
+
+$Promise.prototype.state = 'pending';
+$Promise.prototype.value;
+
+function Deferral()
+{
+	this.$promise = new $Promise;
+}
+
+//Why is the constructor version unique and not this?
+//Deferral.prototype.$promise = new $Promise;
+
+Deferral.prototype.resolve = function(data)
+{
+	if(this.$promise.state === 'pending')
+	{
+		this.$promise.value = data;
+		this.$promise.state = 'resolved';
+	}
+}
+
+Deferral.prototype.reject = function(reason)
+{
+	if(this.$promise.state === 'pending')
+	{
+		this.$promise.value = reason;
+		this.$promise.state = 'rejected';
+	}
+}
+
+function defer()
+{
+	return new Deferral();
+}
 
 
 
